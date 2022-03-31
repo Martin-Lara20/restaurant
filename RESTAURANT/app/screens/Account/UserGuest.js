bbf4530d320061cdc5044f0246b2a6d885bd4a0a
Martin-Lara20 <iinf18.mlaral@itesco.edu.mx>
2022-03-30 19:01:27 -0600

    Login

--- RESTAURANT/app/screens/Account/UserGuest.js
+++ RESTAURANT/app/screens/Account/UserGuest.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import { StyleSheet, View, Text, ScrollView, Image} from 'react-native'
 import { Button } from 'react-native-elements'
+import {useNavigation} from '@react-navigation/native'
 
 export default function UserGuest(){
+
+    const navigation = useNavigation()
     return (
         <ScrollView style ={StyleSheet.container}>
             <Image
@@ -10,6 +13,17 @@ export default function UserGuest(){
                 source={require('../../../assets/img/userguest.jpg')}
             />
             <Text style ={styles.title}>Ingresa a tu perfil</Text>
+            <Text style ={styles.descripcion}>Busca y vizualiza restaurantes</Text>
+
+            <View style = {styles.viewBtn}>
+                <Button
+                    title = 'ver tu perfil'
+                    buttonStyle ={styles.btnStyle}
+                    containerStyle = {styles.btnContainer}
+                    onPress = {()=> navigation.navigate('login')}
+                />
+            </View>
+
         </ScrollView>
     )
 }
@@ -29,5 +43,16 @@ const styles = StyleSheet.create({
         fontSize: 19,
         marginBottom: 10,
         textAlign: 'center'
+    },
+    viewBtn:{
+        flex: 1,
+        alignItems: 'center',
+    },
+    btnStyle:{
+        backgroundColor: '#00a680'
+    },
+    btnContainer:{
+        width: '70%',
+
     }
 })
