337c5dac33fe4baf3ee07d69f7f2104d4bebb276
Martin-Lara20 <iinf18.mlaral@itesco.edu.mx>
2022-03-31 18:13:53 -0600

    Register Form

--- RESTAURANT/app/screens/Account/Register.js
+++ RESTAURANT/app/screens/Account/Register.js
@@ -1,25 +1,28 @@
 import React from "react";
 import {StyleSheet, View, Text, Image} from 'react-native'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
+import RegisterForm from "../../components/Account/RegisterForm";
 
 export default function Register(){
     return(
-        <View>
+        <KeyboardAwareScrollView>
             <Image
                 source={require('../../../assets/img/logo.png')}
                 resizeMode = 'contain'
                 style = {styles.logo}
             />
             <View style ={styles.viewForm}>
-                <Text>Formulario de registro</Text>
+                <RegisterForm/>
             </View>
-        </View>
+        </KeyboardAwareScrollView>
     )
 }
 
 const styles = StyleSheet.create({
     viewForm:{
         marginRight: 40,
-        marginleft: 40,
+        marginLeft: 40,
     },
     logo:{
         width:'100%',
