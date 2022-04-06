d7b50ed7660b553c8e0254ab7ead61fada1959fb
Martin-Lara20 <iinf18.mlaral@itesco.edu.mx>
2022-04-06 18:47:41 -0500

    User Information

--- RESTAURANT/app/screens/Account/UserLogged.js
+++ RESTAURANT/app/screens/Account/UserLogged.js
@@ -1,14 +1,58 @@
-import React from 'react'
-import {View, Text, Button} from 'react-native'
+import React, {useState, useRef, useEffect} from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+import {Button} from "react-native-elements"
 import firebase from 'firebase'
+import Toast from 'react-native-toast-message'
+
+import InforUser from '../../components/Account/InfoUser'
 
 export default function UserLogged(){
+    const[userInfo, setUserInfo] = useState(null)
+    
+    const toastRef = useRef()
+
+    /* () hace referencaia a que es anonima */
+    /* Aqui mandamos a llamar la info de firebase  */
+    useEffect( ()=>{
+        (async()=>{
+            const user = await firebase.auth().
+            currentUser
+            setUserInfo(user)
+        })()
+    },[])
+
     return (
-        <View>
-            <Text>
-                UserLogged
-            </Text>
-            <Button title='Cerrar Sesión' onPress={()=>firebase.auth().signOut()}/>
+        <View style ={styles.viewUserInfo}>
+           {/* UserInfor es la constante del hot de estado */} 
+           {userInfo && <InforUser userInfo={userInfo}/>}
+            <Text>Email </Text>
+            <Button 
+                buttonStyle ={styles.btnCloseSesion}
+                titleStyle={styles.btnClaseSessionText}
+                title='Cerrar Sesión' 
+                onPress={()=>firebase.auth().signOut()}
+            />
+            <Toast ref={toastRef}/>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    viewUserInfo:{
+        minHeight: '100%',
+        backgroundColor: '#f2f2f2'
+    },
+    btnCloseSesion:{
+        marginTop: 30,
+        borderRadius: 0,
+        backgroundColor: '#00a680',
+        borderTopWidth:1,
+        borderBottomColor: '#e3e3e3',
+        borderBottomWidth: 1,
+        paddingTop: 10,
+        paddingBottom: 10,
+    },
+    btnClaseSessionText:{
+        color: '#fff'
+    }
+})
