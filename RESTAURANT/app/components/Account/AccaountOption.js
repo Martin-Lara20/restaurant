94fffa5d481640ffbf45299a29c75356329231ee
Martin-Lara20 <iinf18.mlaral@itesco.edu.mx>
2022-04-10 12:43:47 -0500

    Modal

--- RESTAURANT/app/components/Account/AccaountOption.js
+++ RESTAURANT/app/components/Account/AccaountOption.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import {ListItem, Icon} from 'react-native-elements';
 import { StyleSheet, View, Text } from "react-native";
+import { Modal } from "../Modal";
 
 export default function AccountOption(props){
     const {userInfo, toastRef} = props
+    const [showModal, setShowModal] = useState(false)
+    const [renderComponent, setRenderComponent] =useState(null)
     const selectedComponent = (key) => {
-        console.log('click')
-        console.log(key)
+        switch(key){
+            case 'displayName':
+                setRenderComponent(<Text>Cambiando nombre y apellido</Text>)
+                setShowModal(true)
+                break
+            
+            case 'displayEmail':
+                setRenderComponent(<Text>Cambiando Email</Text>)
+                setShowModal(true)
+                break
+            
+            case 'displayPassword':
+                setRenderComponent(<Text>Cambiando password</Text>)
+                setShowModal(true)
+                break
+            default:
+                setRenderComponent(null)
+                setShowModal(false)
+                break
+
+        }
+
     }
 
     const menuOptions = generatedOptions(selectedComponent)
@@ -21,7 +44,14 @@ export default function AccountOption(props){
                     </ListItem.Content>
                 </ListItem>
             ))}
+
+            {renderComponent && (
+            <Modal isVisible ={showModal} setIsVisible={setShowModal}>
+                {renderComponent}
+            </Modal>
+            )}
         </View>
+        /* Si renderComponent tiene un valor y no es nulo, entonces pasa a renderizar al Modal*/
     )
 }
 
